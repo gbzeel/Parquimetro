cc0ef8c737cb37eef5bd628e36b921dc8b4a5eb5
Gabriel Oliveira <gbzeel@gmail.com>
2025-04-02 18:31:41 -0300

    Alteração de valores

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 class parquimetro {
     constructor(tempo, valor) {
-        this.tempo = parseFloat(tempo);
-        this.valorIncerido = parseFloat(valor).toFixed(2);
+        this.tempo = tempo;
+        this.valorIncerido = valor;
         this.valorPagar = () => {
-            if(this.tempo <= 60) {
-                return this.tempo / 30;
-            }else{
-                return (this.tempo - 30)/30;
+            if(this.tempo == 30) {
+                return 1;
+            }else if (this.tempo == 60) {
+                return this.tempo/60*1.75
+            }else {
+                return this.tempo/60*1.5
             }
         }
-        this.troco = this.valorIncerido - this.valorPagar();
+        this.troco = this.valorIncerido - this.valorPagar().toFixed(2);
     }
 }
 
 function calc() {
-    let tempoEstacionado = document.getElementById("tempo");
+    let tempoEstacionado = document.getElementById("tempo").value.replace('min','');
     let valorParq = document.getElementById("valor");
-    const estacionamento = new parquimetro(tempoEstacionado.value, valorParq.value);
-    document.getElementById("troco").innerHTML = `Troco R$${estacionamento.troco.toFixed(2).replace('.',',')}`;
+    const estacionamento = new parquimetro(tempoEstacionado, valorParq.value);
+    if (valorParq.value == '') {
+        document.getElementById("troco").innerHTML = `Incira um valor`;
+    }else if (estacionamento.troco < 0) {
+        document.getElementById("troco").innerHTML = `Valor insuficiente`;
+    }else {
+        document.getElementById("troco").innerHTML = `Troco R$${estacionamento.troco.toFixed(2).replace('.',',')}`;
+    }
+    console.log(`${valorParq.value}`);
+    
 }
